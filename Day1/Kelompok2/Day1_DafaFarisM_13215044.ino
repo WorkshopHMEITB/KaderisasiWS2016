d69194a59e09ca7a8681c9b17c6a8024667249e0
ArsenicBismuth <d.faris323@gmail.com>
2016-10-01 22:16:59 +0700

    Some fixes

--- Day1/Kelompok2/Day1_DafaFarisM_13215044.ino
+++ Day1/Kelompok2/Day1_DafaFarisM_13215044.ino
@@ -8,28 +8,31 @@
 //Libraries
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>  //Connect I2C [UNO A4(SDA), A5(SCL)]
+//#include <LiquidCrystal.h>
 
 //Set I2C Adress
 LiquidCrystal_I2C lcd(0x38);
+//LiquidCrystal lcd(12, 13, 8, 9, 10, 11); //RS, E, D4, D5, D6, D7
 
 //Hardware specs
 const int backlightpin=13;
-const int butpin[]={4,5,6,7,8}; //Button enter, increase, decrease, pointer left, pointer right
+const int butpin[]={3,4,5,6,7}; //Button enter, increase, decrease, pointer left, pointer right
 const int butcount=sizeof(butpin)/sizeof(int);
-const int ledpin[]={9,10,11};
+const int ledpin[]={9,10,11};   //Must be PWM
 const int ledcount=sizeof(ledpin)/sizeof(int);
 
 //Configs
 const int operand=2;        //Count of numbers to be operated
-int num[operand]={0};       //Zeroing all numbers
+long int num[operand]={0};       //Zeroing all numbers
 const char operators[]={'+','-','x','/'};
-int operatorcnt=sizeof(operators)/sizeof(int);
+int operatorcnt=sizeof(operators)/sizeof(char);
 
 //Var
 int glbstate=0;   //Designating global state
 int calc=0;       //Designating math operation to do
 int pointer=0;    //Designating number pointer
 double ans=0;
+long int iterate = 1; //Based on pointer
 
 struct state {
   bool now;
@@ -38,15 +41,16 @@ struct state {
 
 void setup() {
   for (int i=0;i<ledcount;i++) pinMode(ledpin[i],OUTPUT);
-  pinMode(backlightpin,OUTPUT);
+  //pinMode(backlightpin,OUTPUT);
   for (int i=0;i<butcount;i++) {
     pinMode(butpin[i], INPUT);
     digitalWrite(butpin[i], HIGH);       //Turn on pullup resistors, no additional resistor needed. Remember that 0 means pressed.
   }
-  
+
+  Serial.begin(9600);
   lcd.begin(16, 2);                     //Set up the LCD's number of columns and rows
   digitalWrite(backlightpin,HIGH);
-  
+
   // Print a message to the LCD.
   lcd.print("WSCalc 20160 Calculator");
   lcd.setCursor(0, 1);                  //Realign cursor to the 1st col and 2nd row
@@ -55,10 +59,22 @@ void setup() {
 
 void loop() {
   //Read current button condition
-  for (int i=0;i<butcount;i++) butstate[i].now=!digitalRead(butpin[i]); //Inverse condition since using pullup resistor
+  for (int i=0;i<butcount;i++) {
+    butstate[i].now=!digitalRead(butpin[i]); //Inverse condition since using pullup resistor
+    Serial.print(butstate[i].now);
+  }
+  Serial.print(' ');
+  Serial.print(iterate);
+  Serial.print(' ');
+  Serial.print(num[0]);
+  Serial.print(' ');
+  Serial.print(num[1]);
+  Serial.print(' ');
+  Serial.print(glbstate);
+  Serial.println();
 
   //Button 1 - Enter
-  if ((butstate[0].now == HIGH) != butstate[0].prev) {
+  if ((butstate[0].now == HIGH) && (butstate[0].now != butstate[0].prev)) {
     ledoff();
     //Start calculation, Submit 1st number, & Submit 2nd number respectively
     if (glbstate<=2) {
@@ -69,9 +85,9 @@ void loop() {
     //Submit math operation and calculate
     if (glbstate==3) {                        
       loading();
-      ans = (double)(                         //Result based on the selection of operation
+      ans =                         //Result based on the selection of operation
         (calc==0)*(num[0]+num[1]) + (calc==1)*(num[0]-num[1])+
-        (calc==2)*(num[0]*num[1]) + (calc==3)*(num[0]/num[1]));
+        (calc==2)*(num[0]*num[1]) + (calc==3)*((double)num[0]/num[1]);
     }
 
     //Reset sketch only, not peripherals. But it doesn't matter in this case.
@@ -81,10 +97,13 @@ void loop() {
     glbstate++;
   }
 
-  int iterate = powf(10,pointer);
+  iterate = powf(10,pointer);
+
+  //Hardcode, problem: 10^x sometimes return 10^x -1 or 10^x -2 even using custom function using int
+  while ((iterate % 10 != 0) && (iterate!=1)) iterate++;
   
   //Button 2 - Increase
-  if ((butstate[1].now == HIGH) != butstate[1].prev) {
+  if ((butstate[1].now == HIGH) && (butstate[1].now != butstate[1].prev)) {
     if (glbstate>=1 && glbstate<=2){
       if (num[glbstate-1] / iterate !=9) num[glbstate-1]+=iterate;    //Iterate the pointer-th digit of i-th number for every push
       else num[glbstate-1]-=9*iterate;                                //Revert the pointer-th digit to zero if it's 9
@@ -96,7 +115,7 @@ void loop() {
   }
 
   //Button 3 - Decrease
-  if ((butstate[1].now == HIGH) != butstate[1].prev) {
+  if ((butstate[2].now == HIGH) && (butstate[2].now != butstate[2].prev)) {
     if (glbstate>=1 && glbstate<=2){
       if (num[glbstate-1] / iterate !=0) num[glbstate-1]-=iterate;    //Revert the pointer-th digit of i-th number for every push
       else num[glbstate-1]+=9*iterate;                                //Revert the pointer-th digit to 9 if it's 0
@@ -108,12 +127,13 @@ void loop() {
   }
 
   //Button 4 - Pointer left
-  if ((butstate[3].now == HIGH) != butstate[3].prev) {
+  if ((butstate[3].now == HIGH) && (butstate[3].now != butstate[3].prev)) {
     if (glbstate>=1 && glbstate<=2) pointer++;    //Move the pointer left
   }
 
+  
   //Button 5 - Pointer right
-  if ((butstate[4].now == HIGH) != butstate[3].prev) {
+  if ((butstate[4].now == HIGH) && (butstate[4].now != butstate[4].prev)) {
     if (glbstate>=1 && glbstate<=2) pointer--;    //Move the pointer right
   }
 
@@ -137,7 +157,7 @@ void displaynum() { //<Addition> Display current numbers and operation symbol in
    *          111333
    */
   lcd.clear();
-  lcd.rightToLeft();
+  //lcd.rightToLeft();
   lcd.print(num[0]);
   lcd.print(' ');
   lcd.print(operators[calc]);
@@ -151,8 +171,8 @@ void ledoff() { //Turning off all LEDs
   for (int i=0;i<ledcount;i++) digitalWrite(ledpin[i],LOW);
 }
 
-void loading() {  //~1s of fading LEDs
-  for (int i=0;i<1000;i++){
+void loading() {  //~2s of fading LEDs
+  for (int i=0;i<2000;i++){
     analogWrite( ledpin[0],(int)(255*abs(sin(i))) );    //<Needs checking>
     analogWrite( ledpin[1],(int)(255*abs(sin(i+20))) );
     analogWrite( ledpin[1],(int)(255*abs(sin(i+40))) );
